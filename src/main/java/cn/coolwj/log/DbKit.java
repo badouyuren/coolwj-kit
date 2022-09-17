ea005a5058873cce6905fb4fe2894519a5b8fc1a
Nigel <liweijie16@hecom.cn>
2022-09-17 19:41:36 +0800

    init

--- src/main/java/cn/coolwj/log/DbKit.java
+++ src/main/java/cn/coolwj/log/DbKit.java
@@ -23,13 +23,11 @@ package cn.coolwj.log;
  * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
  */
 
-import cn.coolwj.thread.ThreadKit;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -48,9 +46,12 @@ public class DbKit {
 
     public final static ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 
+    private static final int MAX_THREAD = 1000;
+
     public static void main(String[] args) throws Exception {
-        List<Future<String>> futures = new ArrayList<>();
-        Callable<String> task = () -> {
+        List<Future<Long>> futures = new ArrayList<>();
+        Callable<Long> task = () -> {
+            long start = System.currentTimeMillis();
 
             HikariConfig config = new HikariConfig();
             config.setJdbcUrl("jdbc:postgresql://pgm-2zef61o064dsynlrvo.pg.rds.aliyuncs.com:5432/hqcloud");
@@ -63,14 +64,12 @@ public class DbKit {
             try (HikariDataSource ds = new HikariDataSource(config)) {
                 try (Connection conn = ds.getConnection()) {
                     conn.setAutoCommit(false);
-
                     Statement stmt = conn.createStatement();
-                    for (int i = 0; i < 10; i++) {
-                        ResultSet resultSet = stmt.executeQuery("select * from p_file_manage_oss where id < random() and oss_url like '%2%' and oss_size is null");
-                        while (resultSet.next()) {
-                            long id = resultSet.getLong("id");
-                            System.out.println("oss id: " + id);
-                        }
+
+                    ResultSet resultSet = stmt.executeQuery("select * from p_file_manage_oss where id < random() and oss_url like '%2%' and oss_size is null");
+                    while (resultSet.next()) {
+                        long id = resultSet.getLong("id");
+                        System.out.println("oss id: " + id);
                     }
 
                     ResultSet set = stmt.executeQuery("select nextval('SEQ_PAAS_GLOBAL') as id");
@@ -80,32 +79,63 @@ public class DbKit {
                         System.out.println("insert id : " + id);
                     }
 
-                    String sql = """
-                            INSERT INTO p_data (
-                            id, code, meta_id, ent_code, name, l_1, l_2,l_3 ) 
-                            VALUES 
-                            ( %s, '%s', '%s', 'HECOM_CRM', cast(random() as text), cast(power(random(),random()) as text),  cast(cbrt(random()) as text), '%s')
-                            """;
-                    String insertSql = String.format(sql, id, id, id, MaxPrime.prime);
-                    int rs = stmt.executeUpdate(insertSql);
+                    StringBuilder sql = new StringBuilder();
+                    sql.append("INSERT INTO p_data");
+                    sql.append("(id, code, meta_id, ent_code, name, ");
+
+                    int dataCount = 88;
+                    for (int i = 1; i <= dataCount; i++) {
+                        sql.append("s_").append(i).append(", ");
+                    }
+                    for (int i = 1; i <= dataCount; i++) {
+                        sql.append("n_").append(i).append(", ");
+                    }
+                    for (int i = 1; i <= dataCount; i++) {
+                        sql.append("l_").append(i).append(", ");
+                    }
+                    sql.append(" status ,");
+                    sql.append(" content");
+                    sql.append(" ) VALUES ( ");
+                    sql.append(id).append(", ");
+                    sql.append(" '").append(id).append("', ");
+                    sql.append(" '").append(id).append("', ");
+                    sql.append(" 'nigel_test', ");
+                    //name
+                    sql.append(" cast(power(random(), random()) as text), ");
+                    sql.append(" cast(cbrt(random()) as text), ".repeat(dataCount));
+                    sql.append(" exp(random()), ".repeat(dataCount));
+                    sql.append(" ?, ".repeat(dataCount));
+                    sql.append(" 0 ,");
+                    //content
+                    sql.append(" md5(?)");
+                    sql.append(" ) ");
+
+                    String insert = sql.toString();
+                    System.out.println(insert);
+                    PreparedStatement preparedStatement = conn.prepareStatement(insert);
+
+                    String prime = MaxPrime.readPrime();
+                    for (int i = 1; i <= dataCount; i++) {
+                        preparedStatement.setString(i, prime);
+                    }
+                    preparedStatement.setString(dataCount + 1, prime);
+                    int rs = preparedStatement.executeUpdate();
                     System.out.println(rs);
 
-                    ThreadKit.sleep(23);
+
                     conn.commit();
                 }
             }
 
-            return System.currentTimeMillis() + "";
+            return System.currentTimeMillis() - start;
         };
 
-        int loop = 500;
-        while (loop > 0) {
+        for (int i = 0; i < MAX_THREAD; i++) {
             futures.add(EXECUTOR_SERVICE.submit(task));
-            loop--;
         }
-        for (Future<String> future : futures) {
+        for (Future<Long> future : futures) {
             try {
-                System.out.println(future.get());
+                System.out.println("cost : " + future.get() + "ms");
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
