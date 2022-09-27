32fa8a77fccc81d612e2b52c025427cf143b0799
Nigel <liweijie16@hecom.cn>
2022-09-27 09:34:25 +0800

    init

---
+++ src/main/java/cn/coolwj/js/JsRunner.java
@@ -0,0 +1,52 @@
+package cn.coolwj.js;
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the god animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ */
+
+import cn.coolwj.secutity.AESUtils;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.List;
+
+/**
+ * @author Nigel Lee
+ * @date 2022/9/27
+ */
+public class JsRunner {
+    public static void main(String[] args) throws ScriptException {
+        // Graal
+        List<ScriptEngineFactory> list = new ScriptEngineManager().getEngineFactories();
+
+
+        ScriptEngine graalEngine = new ScriptEngineManager().getEngineByName("graal.js");
+        graalEngine.eval("print('Hello World!');");
+
+
+        graalEngine.eval(AESUtils.CRYPTO_JS_CODE);
+        graalEngine.eval(AESUtils.JS_ENCRYPT_CODE);
+
+    }
+}
